140ee87fe56d8a334bff3063861c6c95f4cb783c
devel0624 <devel062@gmail.com>
2024-06-27 18:06:29 +0900

    repository initialize commit

---
+++ src/main/java/com/nhnacademy/lms/auth/CelineAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.lms.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelineAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CelineAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/lms/auth/CelineAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.lms.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelineAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

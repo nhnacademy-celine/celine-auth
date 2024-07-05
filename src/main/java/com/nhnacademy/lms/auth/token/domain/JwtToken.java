1bce51677c4dc66a7020d7fa9302407d19ed0159
devel0624 <devel062@gmail.com>
2024-07-05 17:09:56 +0900

    [Docs] add javadoc
    description : Jwt 토큰 관련 클래스 자바독 추가

--- src/main/java/com/nhnacademy/lms/auth/token/domain/JwtToken.java
+++ src/main/java/com/nhnacademy/lms/auth/token/domain/JwtToken.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.ClaimsBuilder;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwsHeader;
 import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import java.util.Arrays;
@@ -14,16 +15,22 @@ import java.util.Map;
 import java.util.Set;
 
 /**
+ * The type Jwt token.
+ *
  * @author : 이성준
  * @since : 1.0
  */
-
 public class JwtToken extends Token {
 
     private JwsHeader header;
     private Map<String, Object> claims;
     private byte[] signature;
 
+    /**
+     * Instantiates a new Jwt token.
+     *
+     * @param tokenValue . (period) 로 구분되는 header, payload, signature 로 이루어진 jwt 문자열입니다.
+     */
     public JwtToken(String tokenValue) {
         super(tokenValue);
     }
@@ -35,15 +42,31 @@ public class JwtToken extends Token {
         throw new UnverifiedTokenException("검증되지 않은 토큰의 값은 사용할 수 없습니다.");
     }
 
+    /**
+     * Gets claim keys.
+     *
+     * @return the claim keys
+     */
     public Set<String> getClaimKeys() {
         return getClaims().keySet();
     }
 
+    /**
+     * Gets claim.
+     *
+     * @param key the key
+     * @return the claim
+     */
     public Object getClaim(String key) {
         return getClaims().get(key);
     }
 
-    public void verify(JwtParser parser) {
+    /**
+     * Verify Jwt with JwtParser.
+     *
+     * @param parser the parser to parse jwt
+     */
+    public void verify(JwtParser parser) throws JwtException, IllegalArgumentException {
         if (parser.isSigned(tokenValue)) {
             Jws<Claims> jws = parser.parseSignedClaims(tokenValue);
 
@@ -74,41 +97,86 @@ public class JwtToken extends Token {
         private JwtBuilder jwtBuilder;
         private final ClaimsBuilder claimsBuilder;
 
+        /**
+         * Instantiates a new Jwt token builder.
+         */
         public JwtTokenBuilder() {
             this.jwtBuilder = Jwts.builder();
             this.claimsBuilder = Jwts.claims();
         }
 
+        /**
+         * Replace the JwtBuilder for the jwt build.
+         *
+         * @param jwtBuilder the jwt builder
+         * @return the jwt token builder
+         */
         public JwtTokenBuilder jwtBuilder(JwtBuilder jwtBuilder){
             this.jwtBuilder = jwtBuilder;
             return this;
         }
 
+        /**
+         * add Issuer jwt token.
+         *
+         * @param iss the iss
+         * @return the jwt token builder
+         */
         public JwtTokenBuilder issuer(String iss) {
             claimsBuilder.issuer(iss);
             return this;
         }
 
+        /**
+         * add Expiration jwt token.
+         *
+         * @param exp the exp
+         * @return the jwt token builder
+         */
         public JwtTokenBuilder expiration(Date exp) {
             claimsBuilder.expiration(exp);
             return this;
         }
 
+        /**
+         * add IssuedAt jwt token.
+         *
+         * @param iat the iat
+         * @return the jwt token builder
+         */
         public JwtTokenBuilder issuedAt(Date iat) {
             claimsBuilder.issuedAt(iat);
             return this;
         }
 
+        /**
+         * Add Id for jwt token.
+         *
+         * @param jti the jti
+         * @return the jwt token builder
+         */
         public JwtTokenBuilder id(String jti) {
             claimsBuilder.id(jti);
             return this;
         }
 
+        /**
+         * Add jwt token claim.
+         *
+         * @param key   the key
+         * @param value the value
+         * @return the jwt token builder
+         */
         public JwtTokenBuilder add(String key, Object value) {
             claimsBuilder.add(key, value);
             return this;
         }
 
+        /**
+         * Build jwt token.
+         *
+         * @return the jwt token
+         */
         public JwtToken build() {
             return new JwtToken(jwtBuilder.claims(claimsBuilder.build()).compact());
         }
--- src/main/java/com/nhnacademy/lms/auth/token/domain/Token.java
+++ src/main/java/com/nhnacademy/lms/auth/token/domain/Token.java
@@ -3,21 +3,33 @@ package com.nhnacademy.lms.auth.token.domain;
 import lombok.Getter;
 
 /**
+ * The type Token.
+ *
  * @author : 이성준
  * @since : 1.0
  */
-
 @Getter
 public abstract class Token {
 
     private boolean isVerified;
     protected final String tokenValue;
 
+
+    /**
+     * Instantiates a new Token.
+     *
+     * @param tokenValue the token value
+     */
     protected Token(String tokenValue) {
         this.tokenValue = tokenValue;
         this.isVerified = false;
     }
 
+
+    /**
+     * Verify.
+     * 토큰 객체의 상태를 검증되었음 으로 변경합니다.
+     */
     protected void verify(){
         this.isVerified = true;
     }
